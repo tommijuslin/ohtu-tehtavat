1822d38778c5a7029597b2fd3bf2ed88a5e0bbfc
tommijuslin <tommi.juslin@gmail.com>
2021-11-26 14:22:30 +0200

    viikko4 ex5 test12

--- viikko4/tdd-ostoskori/src/index.py
+++ viikko4/tdd-ostoskori/src/index.py
@@ -1,1 +1,16 @@
 # testikoodi tänne jos tarvetta
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+
+def main():
+    kori = Ostoskori()
+    maito = Tuote("Maito", 3)
+    kori.lisaa_tuote(maito)
+    kori.lisaa_tuote(maito)
+
+    print(len(kori.ostokset()))
+
+
+if __name__ == "__main__":
+    main()
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -87,3 +87,13 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostos_jolla_oikea_tuotteen_nimi_ja_maara(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.tuote.nimi(), maito.nimi())
+        self.assertEqual(ostos.lukumaara(), 2)
