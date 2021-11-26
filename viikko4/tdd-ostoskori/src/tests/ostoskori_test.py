5f7fd9af65d50f3d3d306f9d03b2e0100f87e94f
tommijuslin <tommi.juslin@gmail.com>
2021-11-26 14:47:16 +0200

    viikko4 ex5 test14

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -107,3 +107,13 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
 
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_jos_koriin_lisatty_tuote_ja_tuote_poistetaan_on_kori_tyhja(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 0)
+        self.assertEqual(self.kori.hinta(), 0)
