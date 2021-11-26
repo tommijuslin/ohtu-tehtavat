97b3d1a69fa9d4a732023b45862c14872164f32e
tommijuslin <tommi.juslin@gmail.com>
2021-11-26 12:42:52 +0200

    viikko4 ex5 test5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -29,3 +29,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(porkkana)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        porkkana = Tuote("Porkkana", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(porkkana)
+
+        self.assertEqual(self.kori.hinta(), 5)
