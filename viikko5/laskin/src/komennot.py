fe01900ab3f5fae954745e1b662d7839f10c1b6a
tommijuslin <tommi.juslin@gmail.com>
2021-12-01 15:26:59 +0200

    viikko5 ex4

---
+++ viikko5/laskin/src/komennot.py
@@ -0,0 +1,27 @@
+class Summa:
+    def __init__(self, sovellus, io):
+        self._sovellus = sovellus
+        self._io = io
+
+    def suorita(self):
+        luku = int(self._io())
+        self._sovellus.plus(luku)
+
+
+class Erotus:
+    def __init__(self, sovellus, io):
+        self._sovellus = sovellus
+        self._io = io
+
+    def suorita(self):
+        luku = int(self._io())
+        self._sovellus.miinus(luku)
+
+
+class Nollaus:
+    def __init__(self, sovellus, io):
+        self._sovellus = sovellus
+        self._io = io
+
+    def suorita(self):
+        self._sovellus.nollaa()
